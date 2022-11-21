013ca424f0eb46e9c0ecb7ba722c1c4cb34ba996
patrickhoey20 <patrickhoey2024@u.northwestern.edu>
2022-11-20 19:18:56 -0600

    migrating project to github pages

---
+++ main.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+    var coll = document.getElementsByClassName("collapsible"); // get array of collapsible buttons
+    var i;
+    for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() { // on click of any of the cololapsible buttons
+        var content = this.nextElementSibling; // the content to be shown
+        if (content.style.maxHeight){ // if currently showing, hide content
+            content.style.maxHeight = null;
+            this.textContent = 'See More'; // change text of button
+        } else { // else (not currently showing), show content
+            content.style.maxHeight = content.scrollHeight + "px";
+            this.textContent = 'See Less'; // change text of button
+        }
+    });
+    }
+}
